228562297cf2e764ae81289a139f05f43dda12be
luwt <lwtcookie@gmail.com>
2025-03-25 22:53:35 +0800

    优化之使用splitChunks拆包，减少包的体积

--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,59 @@ const webpackBaseConfig = {
       }
     ]
   },
+  optimization: {
+    runtimeChunk: {
+      name: 'runtime'
+    },
+    splitChunks: {
+      chunks: 'all',
+      // maxInitialRequests: 3, // 解释：表示在入口点处的最大并行请求数。
+      // name: true, // 默认是chunk的名字，可以自定义
+      // maxAsyncRequests: 3, // 解释：这个属性用于控制异步加载时的最大请求数量。如果某个模块被异步加载的次数超过了3次，那么它会被单独打包，以减少异步加载时的请求数量。
+      cacheGroups: {
+        commons: {
+          chunks: 'all', //表示这个公共模块可以从所有入口点提取。
+          name: 'chunk-common', // 指定生成的公共模块的名称。
+          minChunks: 2, // 表示被引用次数，默认为1
+          maxInitialRequests: 5, // 表示在入口点处的最大并行请求数。
+          priority: 1, // 优先级，数字越大，优先级越高
+          reuseExistingChunk: true, // 表示是否重用已经存在的公共模块。
+          enforce: true // 表示强制提取模块。
+        },
+        vendors: {
+          name: 'chunk-vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 2,
+          reuseExistingChunk: true,
+          enforce: true
+        },
+        uiComponent: {
+          name: 'chunk-component',
+          test: /([\\/]node_modules[\\/]@mui[\\/].+\w)|(src[\\/]components[\\/]common)|([\\/]node_modules[\\/]@yideng[\\/]components)/,
+          chunks: 'all',
+          priority: 4, // 优先级，数字越大，优先级越高
+          reuseExistingChunk: true,
+          enforce: true
+        },
+        ethersSDK: {
+          name: 'chunk-ethers-sdk',
+          test: /[\\/]node_modules[\\/](ethers*\w|@ethersproject*\w|@web3-react*\w)/,
+          chunks: 'all',
+          priority: 5, // 优先级，数字越大，优先级越高
+          reuseExistingChunk: true,
+          enforce: true
+        },
+        reaactLibs: {
+          name: 'chunk-react-libs',
+          test: /[\\/]node_modules[\\/](react*\w|react-dom*\w|react-router*\w)/,
+          chunks: 'all',
+          priority: 6, // 优先级，数字越大，优先级越高
+          reuseExistingChunk: true,
+          enforce: true
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': resolve('src/'),
